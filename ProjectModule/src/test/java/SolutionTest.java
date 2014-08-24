6a2e5472bbc365230ec407d78b73f1a0a7c580e6
Neil <origineil02@gmail.com>
2014-08-23 23:54:51 -0500

    Establising Project Euler template branch

---
+++ ProjectModule/src/test/java/SolutionTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  @Test
+  public void testSample() {
+
+    final String input = "";
+    
+    assertEquals("", new Solution.ClassName().solve(new Scanner(input)));
+  }
+}
