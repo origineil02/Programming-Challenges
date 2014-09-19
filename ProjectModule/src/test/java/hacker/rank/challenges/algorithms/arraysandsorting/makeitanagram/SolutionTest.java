0628cc208db0e8259b5211019682d3931913909b
Neil <origineil02@gmail.com>
2014-09-18 21:20:52 -0500

    Hacker Rank : Algorithms : Arrays and Sorting : Make It Anagram

---
+++ ProjectModule/src/test/java/hacker/rank/challenges/algorithms/arraysandsorting/makeitanagram/SolutionTest.java
@@ -0,0 +1,13 @@
+package hacker.rank.challenges.algorithms.arraysandsorting.makeitanagram;
+
+import java.util.Scanner;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class SolutionTest {
+  
+   @Test
+  public void testSample() {
+    assertEquals("4", new Solution.MakeItAnagram().solve(new Scanner("abc\ncde")));
+  }
+}
