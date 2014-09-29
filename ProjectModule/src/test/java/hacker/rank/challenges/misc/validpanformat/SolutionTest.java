bfb216fa6ef2793e465967ffbeb61a2a0dc50c91
Neil <origineil02@gmail.com>
2014-09-28 21:48:39 -0500

    Hacker Rank : Misc : Valid PAN format

---
+++ ProjectModule/src/test/java/hacker/rank/challenges/misc/validpanformat/SolutionTest.java
@@ -0,0 +1,19 @@
+package hacker.rank.challenges.misc.validpanformat;
+
+import java.util.Scanner;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  @Test
+  public void testSample() {
+    final String input = "3\n"
+            + "ABCDS1234Y\n"
+            + "ABAB12345Y\n"
+            + "avCDS1234Y";
+
+    assertEquals("YES\\nNO\\nNO\\n", new Solution.ValidPanFormat().solve(new Scanner(input)));
+  }
+
+}
