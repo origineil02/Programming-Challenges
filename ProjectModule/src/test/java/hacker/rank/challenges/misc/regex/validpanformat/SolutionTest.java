895cc27dac47baf3a6dacb93ba32a65c61e1d0ec
Neil <origineil02@gmail.com>
2014-12-10 22:18:23 -0600

    Hacker Rank :  refactor misnamed package

--- ProjectModule/src/test/java/hacker/rank/challenges/misc/regex/validpanformat/SolutionTest.java
+++ ProjectModule/src/test/java/hacker/rank/challenges/misc/regex/validpanformat/SolutionTest.java
@@ -1,6 +1,5 @@
-package hacker.rank.challenges.misc.validpanformat;
+package hacker.rank.challenges.misc.regex.validpanformat;
 
-import hacker.rank.challenges.misc.regex.validpanformat.Solution;
 import java.util.Scanner;
 import static junit.framework.Assert.assertEquals;
 import org.junit.Test;
