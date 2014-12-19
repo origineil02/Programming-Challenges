3785d6ee49ff1f10d96312cb06b68e0d1870bd68
Neil <origineil02@gmail.com>
2014-12-18 18:45:14 -0600

    Hacker Rank : Linux Shell : Text Processing : grep #2

--- ProjectModule/src/main/java/hacker/rank/challenges/linuxshell/bash/text/processing/Command-grep-Challenges.sh
+++ ProjectModule/src/main/java/hacker/rank/challenges/linuxshell/bash/text/processing/Command-grep-Challenges.sh
@@ -1,2 +1,5 @@
 #1 - use grep to display all those lines which contain the word the in them
 grep "\bthe\b" $1
+
+#2 - use grep to display all those lines which contain the word the in them. The search should NOT be sensitive to case.
+grep -i "\bthe\b" $1
