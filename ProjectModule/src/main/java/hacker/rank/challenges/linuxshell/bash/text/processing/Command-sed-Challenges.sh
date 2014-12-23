11dd94a4370a792ce075194d6fde2fb37d15c539
Neil <origineil02@gmail.com>
2014-12-23 14:24:23 -0600

    Hacker Rank :  The Linux Shell : Intro to Text Processing Utilities : comments

--- ProjectModule/src/main/java/hacker/rank/challenges/linuxshell/bash/text/processing/Command-sed-Challenges.sh
+++ ProjectModule/src/main/java/hacker/rank/challenges/linuxshell/bash/text/processing/Command-sed-Challenges.sh
@@ -3,3 +3,5 @@ sed s/"\bthe\b"/this/ $1
 
 #2 - transform all the occurrences of the word 'thy' with 'your'. The search should be case insensitive
 sed s/[tT]hy/your/g  $1
+
+#3 - highlight all the occurrences of 'thy' by wrapping them up in brace brackets. The search should be case-insensitive.
