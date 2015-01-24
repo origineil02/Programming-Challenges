138f1029a39348af478bed9d17cbba3932e6ddf9
Neil <origineil02@gmail.com>
2015-01-23 21:29:45 -0600

    Hacker Rank : Algorithms :  Summations and Algebra : Triangle Numbers

---
+++ ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/Solution.java
@@ -0,0 +1,38 @@
+package attempt.hacker.rank.challenges.algorithms.summationandalgebra.trianglenumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+  
+  public static class TriangleNumbers implements Runnable {
+
+    private List<List<Integer>> getTriangle(){
+      for(int i = 0; i < 1000000000; ++i){
+        
+      }
+      return new ArrayList();
+    }
+    
+    public String solve(final Scanner in) {
+      
+      final StringBuilder sb = new StringBuilder();
+      
+      int testCases = in.nextInt();
+      while(0 < testCases--){
+        
+      }
+      return sb.toString();
+    }
+    
+    @Override
+    public void run() {
+      System.out.println(solve(new Scanner(System.in)));
+    }
+  }
+  
+  public static void main(String[] args){
+    new Thread(new TriangleNumbers()).run();
+  }
+}
---
+++ ProjectModule/src/test/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/SolutionTest.java
@@ -0,0 +1,13 @@
+package attempt.hacker.rank.challenges.algorithms.summationandalgebra.trianglenumbers;
+
+import java.util.Scanner;
+import junit.framework.TestCase;
+
+public class SolutionTest extends TestCase {
+  
+  public void testSample() {
+    new Solution.TriangleNumbers().solve(new Scanner("3"));
+     assertTrue(true);
+  }
+  
+}
