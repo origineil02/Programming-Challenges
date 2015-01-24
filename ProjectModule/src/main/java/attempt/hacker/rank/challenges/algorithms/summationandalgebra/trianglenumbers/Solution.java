68c3aa31a103a3f67645a9cf2b549e242f773771
Neil <origineil02@gmail.com>
2015-01-23 22:10:41 -0600

    Hacker Rank : Algorithms :  Summations and Algebra : Triangle Numbers

--- ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/Solution.java
+++ ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/Solution.java
@@ -1,18 +1,36 @@
 package attempt.hacker.rank.challenges.algorithms.summationandalgebra.trianglenumbers;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution {
   
   public static class TriangleNumbers implements Runnable {
 
-    private List<List<Integer>> getTriangle(){
-      for(int i = 0; i < 1000000000; ++i){
+    private int getData(int target){
+      
+      if(1 == target || 2 == target){return -1;}
+      
+      final LinkedList<Integer> row = new LinkedList();
+      row.add(1);
+      row.add(target-1);
+      
+      final LinkedList<Integer> previous = new LinkedList();
+      previous.add(0);
+      previous.add(1);
+      previous.add(target-2);
+      
+      int length = target%2==0 ? target+1 : target;
+       
+      for(int i = 1; i < length; ++i){
         
+        int value = previous.get(i) + previous.get(i-1) + previous.get(i+1);
+        previous.add(value); //wrong
+        row.add(value);
+        
+        if(value%2==0){ System.out.println("--" + target + "--\n" +previous + "\n" + row + "\n-----\n");return i+1;}
       }
-      return new ArrayList();
+      return -1;
     }
     
     public String solve(final Scanner in) {
@@ -21,9 +39,10 @@ public class Solution {
       
       int testCases = in.nextInt();
       while(0 < testCases--){
-        
+        int row = in.nextInt();
+        sb.append(getData(row)).append("\n");
       }
-      return sb.toString();
+      return sb.toString().trim();
     }
     
     @Override
--- ProjectModule/src/test/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/SolutionTest.java
+++ ProjectModule/src/test/java/attempt/hacker/rank/challenges/algorithms/summationandalgebra/trianglenumbers/SolutionTest.java
@@ -6,8 +6,18 @@ import junit.framework.TestCase;
 public class SolutionTest extends TestCase {
   
   public void testSample() {
-    new Solution.TriangleNumbers().solve(new Scanner("3"));
-     assertTrue(true);
+    System.out.println(Integer.MAX_VALUE);
+    assertEquals("2\n3", new Solution.TriangleNumbers().solve(new Scanner("2\n3\n4")));
   }
+  
+  public void testProblemStatement() {
+    assertEquals("-1\n-1", new Solution.TriangleNumbers().solve(new Scanner("2\n1\n2")));
+    
+    for(int i = 5; i < 10; ++i){
+      System.out.println(new Solution.TriangleNumbers().solve(new Scanner("1\n"+i)));
+    }
+    
+  }
+  
   
 }
