61b10e0d6b340218b25dbe877e577225e8ed993d
Neil <origineil02@gmail.com>
2015-02-12 22:59:15 -0600

    Hacker Rank : Algorithms : Search : Mr K Marsh : passing samples but failing other test cases

--- ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/Solution.java
+++ ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/Solution.java
@@ -8,6 +8,38 @@ public class Solution {
   
   public static class MrKMarsh implements Runnable {
 
+    private boolean isObstructed(int column, List<String> fields){
+      for (String string : fields) {
+        if(string.toCharArray()[column] == 'x'){
+          return true;
+        }
+      }
+      return false;
+    }
+    
+    private String getDimensions(int N, int S, int E, int W, List<String> field){
+      
+      if(N >= S || E >= W){return "impossible";}
+      
+      if(field.get(N).contains("x")){
+        return getDimensions(N+1, S, E, W, field);
+      }
+      
+      if(field.get(S).contains("x")){
+        return getDimensions(N, S-1, E, W, field);
+      }
+      
+      if(isObstructed(E, field.subList(N, S))){
+        return getDimensions(N, S, E+1, W, field);
+      }
+        
+      if(isObstructed(W, field.subList(N, S))) {
+        return getDimensions(N, S, E, W-1, field);
+      }
+      
+      return String.valueOf(2 * (W - E) +  2 * (S - N));
+    }
+    
     public String solve(final Scanner in) {
       
       final int height = in.nextInt();
@@ -19,7 +51,7 @@ public class Solution {
         field.add(in.nextLine());
       }
       
-      return field.toString();
+      return getDimensions(0, height-1, 0, width-1, field);
     }
     
     @Override
--- ProjectModule/src/test/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/SolutionTest.java
+++ ProjectModule/src/test/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/SolutionTest.java
@@ -5,14 +5,29 @@ import junit.framework.TestCase;
 
 public class SolutionTest extends TestCase {
 
-  public void testSample() {
+  public void testSample1() {
     final String input = "4 5\n"
             + ".....\n"
             + ".x.x.\n"
             + ".....\n"
             + ".....";
 
-    assertEquals("[....., .x.x., ....., .....]", new Solution.MrKMarsh().solve(new Scanner(input)));
+    assertEquals("14", new Solution.MrKMarsh().solve(new Scanner(input)));
   }
 
+  public void testSample2() {
+    final String input = "2 2\n"
+            + ".x\n"
+            + "x.";
+
+    assertEquals("impossible", new Solution.MrKMarsh().solve(new Scanner(input)));
+  }
+
+  public void testSample3() {
+    final String input = "2 5\n"
+            + ".....\n"
+            + "xxxx.";
+
+    assertEquals("impossible", new Solution.MrKMarsh().solve(new Scanner(input)));
+  }
 }
