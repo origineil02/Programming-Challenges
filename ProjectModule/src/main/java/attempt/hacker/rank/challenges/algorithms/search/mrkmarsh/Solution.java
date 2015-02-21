5c85adf9607e5ac72411858efdb18e1f44a493d3
Neil <origineil02@gmail.com>
2015-02-20 22:22:47 -0600

    Hacker Rank : Algorithms : Search : Mr K Marsh : begin new approach

--- ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/Solution.java
+++ ProjectModule/src/main/java/attempt/hacker/rank/challenges/algorithms/search/mrkmarsh/Solution.java
@@ -28,81 +28,42 @@ public class Solution {
         result = Math.max(result, v);
       }
     }
-
-    private String getScopedData(int index, int width, final String data){
-      
-      if(index == 0 && width == data.length()-1){
-        return data;
-      }
-      
-      final StringBuilder sb = new StringBuilder();
-        for (int j = index, k = 0; k < width; ++j, ++k) {
-          sb.append(data.charAt(j));
-        }
-        return  sb.toString();
+    
+    private String getData(Dimension d, int row, int index, List<String> field){
+        final String rowData = field.get(row);
+        return rowData.substring(index).substring(0, d.width + 1);
     }
     
     private void process(Dimension d, int row, int index, List<String> field, final ResultContainer rc) {
 
-      if (d.height < 2 || d.width < 2) {
-        return;
+      //log("-begin-");
+      
+      if(d.width > field.get(row).length()-1){ return; }
+      if(d.height > field.size()) { return; }
+      
+      final String first = getData(d, row, index, field);
+      log(first);
+      
+      if(!first.contains("x")){
+        process(new Dimension(d.height, d.width+1), row, index, field, rc);
+      }
+         
+      log("----");
+      for (int i = 1; i < d.height; i++) {
+        final String next = getData(d, row+i, index, field);
+        log(next);
+        
+        if(next.contains("x") && i == d.height){
+          if(next.charAt(0) != 'x' && next.charAt(next.length()-1) != 'x'){
+            process(new Dimension(d.height+1, d.width), row, index, field, rc);
+          }
+          return;
+        }else if(d.height < field.size()){
+          process(new Dimension(d.height+1, d.width), row, index, field, rc);
+        }
       }
       
-      if(rc.result > 0){
-        int potential = 2 * (d.width - 1) + 2 * (d.height - 1);
-        if(potential < rc.result){
-          return;
-        }
-      }
-
-      final List<String> rows = new ArrayList<>();
-      for (int i = row, j = 0; j < d.height; i++, j++) {
-        rows.add(field.get(i));
-      }
-
-      log("Process: " + row + " | " + index + " D (w|h): " + d.width + " | " + d.height);
-
-      for (int i = 0; i < rows.size(); i++) {
-
-        String rowData = getScopedData(index, d.width, rows.get(i));
-
-        if (rowData.length() < 2) {
-          return;
-        }
-
-        log(rowData + " row " + (i+1) + " out of " + rows.size());
-
-        final String data = rowData.charAt(0) + "" + rowData.charAt(rowData.length() - 1);
-        if (i == 0 || i == rows.size() - 1) {
-          if (rowData.contains("x")) {
-            if (i == rows.size() - 1) {
-              process(new Dimension(d.height - 1, d.width), row, index, field, rc);
-            } //else if (rowData.charAt(rowData.length() - 1) == 'x') {
-              process(new Dimension(d.height, d.width - 1), row, index, field, rc);
-            //}
-            return;
-          }
-        }
-        else {
-          
-          if (data.contains("x")) {
-            log(" edge fail");
-            process(new Dimension(d.height, d.width - 1), row, index, field, rc);
-            if(i < rows.size()-1 && i > 1){
-              final String previous = getScopedData(index, d.width, rows.get(i-1));
-              if(!previous.contains("x")){
-                int result = 2 * (d.width - 1) + 2 * (i-1);
-                log("Result: " + result);
-                rc.setResult(result);
-              }
-              //  process(new Dimension(d.height-1, d.width), row, index, field, rc);
-            }
-            
-            return;
-          }
-        }
-      }
-
+      log("----");
       int result = 2 * (d.width - 1) + 2 * (d.height - 1);
       log("Result: " + result);
       rc.setResult(result);
@@ -124,7 +85,7 @@ public class Solution {
       for (int i = 0; i < field.size() - 1; ++i) {
         final String row = field.get(i);
         for (int j = 0; j < row.length() - 1; ++j) {
-          process(new Dimension(dimensions.height - i, dimensions.width - j), i, j, field, rc);
+          process(new Dimension(1, 1), i, j, field, rc);
         }
       }
 
@@ -132,7 +93,7 @@ public class Solution {
     }
 
     private void log(String msg) {
-      //System.out.println(msg);
+      System.out.println(msg);
     }
 
     @Override
